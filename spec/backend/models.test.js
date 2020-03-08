917dd286a6cd5f99940085a7eef9badf412e70d5
Myron <myron.yook@gmail.com>
2020-03-08 16:02:56 -0400

    test for CI/CD expect fail

--- spec/backend/models.test.js
+++ spec/backend/models.test.js
@@ -5,3 +5,8 @@ test("Form.all() should return an array", async () =>{
   const result = await Form.all() ||  [];
   expect(result.length).toEqual(1);
 });
+
+test("Form.all() should return an array", async () =>{
+  const result = await Form.all() ||  [];
+  expect(result.length).toEqual(5);
+});
